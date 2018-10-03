cd6e34f2ce2ab3bda6b4c3af478fab63bb544760
Yicheng Wang <alex.wyc2098@protonmail.ch>
2018-10-02 22:38:37 -0400

    step2

--- FlaskWorkshop/src/app.py
+++ FlaskWorkshop/src/app.py
@@ -2,9 +2,15 @@ from flask import Flask
 
 app = Flask(__name__)
 
+x = 0
+
 @app.route("/")
 def root():
     return "hello world"
+
+@app.route("/show/")
+def show():
+    return "x is " + str(x)
 
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000, debug = True)
