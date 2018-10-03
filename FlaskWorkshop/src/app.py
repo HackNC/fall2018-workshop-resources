cde21ec3336e8f1f99fc955397d8baf742145159
Yicheng Wang <alex.wyc2098@protonmail.ch>
2018-10-02 22:39:43 -0400

    step3

--- FlaskWorkshop/src/app.py
+++ FlaskWorkshop/src/app.py
@@ -12,5 +12,17 @@ def root():
 def show():
     return "x is " + str(x)
 
+@app.route("/incr/")
+def incr():
+    global x
+    x = x + 1
+    return "x is now " + str(x)
+
+@app.route("/reset/")
+def reset():
+    global x
+    x = 0
+    return "x has been reset"
+
 if __name__ == "__main__":
     app.run(host = "0.0.0.0", port = 8000, debug = True)
